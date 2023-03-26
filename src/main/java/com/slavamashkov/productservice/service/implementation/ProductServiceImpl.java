16e72c89bf63c691914c45256d07b093236b6054
DayDream-21 <vova13158@gmail.com>
2023-03-26 17:57:43 +0300

    Start a building microservice project

---
+++ src/main/java/com/slavamashkov/productservice/service/implementation/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.slavamashkov.productservice.service.implementation;
+
+import com.slavamashkov.productservice.dto.ProductRequest;
+import com.slavamashkov.productservice.dto.ProductResponse;
+import com.slavamashkov.productservice.model.Product;
+import com.slavamashkov.productservice.repository.ProductRepository;
+import com.slavamashkov.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product with id: {} is saved", product.getId());
+    }
+
+    @Override
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductResponse::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+}
